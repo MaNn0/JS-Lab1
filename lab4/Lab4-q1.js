75bc97da673a259168f2ad5d95de1a52035c37f9
Abdelrahman Ebeid <71596240+MaNn0@users.noreply.github.com>
2024-12-27 22:59:44 +0200

    Update Lab4-q1.js

--- lab4/Lab4-q1.js
+++ lab4/Lab4-q1.js
@@ -1,19 +1,17 @@
-// var slide = document.getElementById("image");
-// slide.addEventListener("mouseover", setimg);
-// slide.addEventListener("mouseout", mouseOut);
-// let i = 1
-// function mouseOver(){
-//     slide.src = `./Untitled${i++}.jpeg`;
-//     if(i === 6) i = 1;
-// };
-// var yes;
-// function setimg(){
-//     yes = setInterval(mouseOver,1000)
-// }
-// function mouseOut(){
-        
-//     clearInterval(yes)
-//     i=1
-//     slide.src = `./Untitled1.jpeg`;
-        
-// }
+var slide = document.getElementById("image");
+slide.addEventListener("mouseover", setimg);
+slide.addEventListener("mouseout", mouseOut);
+let i = 1
+function mouseOver(){
+     slide.src = `./Untitled${i++}.jpeg`;
+     if(i === 6) i = 1;
+ };
+ var yes;
+ function setimg(){
+     yes = setInterval(mouseOver,1000)
+ }
+ function mouseOut(){
+     clearInterval(yes)
+     i=1
+     slide.src = `./Untitled1.jpeg`;
+ }
